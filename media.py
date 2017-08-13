0f2382890479840f103137196581a754d544c241
Sung <sungp2k@gmail.com>
2017-08-13 07:31:52 -0700

    style change

--- media.py
+++ media.py
@@ -11,13 +11,13 @@ class Movie():
         trailer_youtube_url: trailer link
     """
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_story, poster_image, trailer_url):
         """ initializes all the relevant info of the Movie."""
         self.title = movie_title
-        self.storyline = movie_storyline
+        self.storyline = movie_story
         self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
-        
+        self.trailer_youtube_url = trailer_url
+ 
     def show_trailer(self):
         """ when invoked, it opens the trailer window on the system browser."""
         webbrowser.open(self.trailer_youtube_url)
